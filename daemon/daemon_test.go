c9fc7eccd92b14317d1b36e3a1f46c208de80cb6
hhiroshell <hhiroshell@gmail.com>
2022-09-02 10:03:44 +0900

    tests of daemon.Daemon

--- daemon/daemon_test.go
+++ daemon/daemon_test.go
@@ -1,57 +1,86 @@
 package daemon
 
 import (
+	"context"
 	"errors"
 	"io/fs"
 	"os"
-	"path/filepath"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
 )
 
-var _ = Describe("Socket", func() {
-	var (
-		sock *Socket
-		home string
-	)
+var _ = Describe("Daemon", func() {
+	Describe("func Run()", Ordered, func() {
 
-	BeforeEach(func() {
-		var err error
-		sock, err = NewSocket()
-		Expect(err).NotTo(HaveOccurred())
+		BeforeAll(func() {
+			originalKubebuilderAssets := os.Getenv("KUBEBUILDER_ASSETS")
+			DeferCleanup(func() {
+				err := os.Setenv("KUBEBUILDER_ASSETS", originalKubebuilderAssets)
+				Expect(err).NotTo(HaveOccurred())
+			})
 
-		home, err = os.UserHomeDir()
-		Expect(err).NotTo(HaveOccurred())
-	})
-
-	Describe("func Path()", func() {
-		It("should return \"$HOME/.kube-boat/daemon.socket\"", func() {
-			path := sock.Path()
-			Expect(path).Should(Equal(filepath.Join(home, ".kube-boat", "daemon.socket")))
+			err := os.Setenv("KUBEBUILDER_ASSETS", "/Users/hirhayak/Library/Application Support/io.kubebuilder.envtest/k8s/1.24.2-darwin-arm64")
+			Expect(err).NotTo(HaveOccurred())
 		})
-	})
 
-	Describe("func Close()", func() {
-		When("the kube-boat socket is not exist", func() {
-			It("ends up with error", func() {
-				err := sock.Close()
-				Expect(err).To(HaveOccurred())
-				Expect(err.Error()).Should(HavePrefix("failed remove unix socket file: "))
+		AfterAll(func() {
+		})
+
+		When("kube-boat daemon is started with no optional arguments", Ordered, func() {
+			var (
+				sock    *Socket
+				daemon  *Daemon
+				testenv *envtest.Environment
+			)
+
+			BeforeAll(func() {
+				sock = &Socket{path: "./kube-boat-test-daemon.sock"}
+				testenv = &envtest.Environment{}
+				daemon = NewDaemon(sock, testenv)
+			})
+
+			It("starts and stops without any problems", func() {
+				ctx, cancel := context.WithCancel(context.Background())
+				go func() {
+					err := daemon.Run(ctx, cancel)
+					Expect(err).NotTo(HaveOccurred())
+				}()
+
+				By("checking Kubernetes API Server is available")
+
+				Eventually(func() bool {
+					return testenv.Config != nil
+				}, 10, 1).Should(BeTrue())
+
+				Eventually(func() error {
+					k8s, err := client.New(testenv.Config, client.Options{Scheme: scheme.Scheme})
+					if err != nil {
+						return err
+					}
+
+					err = k8s.Get(context.Background(), client.ObjectKey{Name: "default"}, &corev1.Namespace{})
+					return err
+				}, 20, 1).Should(Succeed())
+
+				By("checking kube-boat daemon endpoints are available")
+
+				By("checking all resources are deleted")
+
+				// TODO: shutting down with the daemon endpoint
+				cancel()
+
+				// TODO: check the API Server is not running
+				Eventually(func() bool {
+					_, err := os.Stat(sock.Path())
+					return errors.Is(err, fs.ErrNotExist)
+				}, 20, 1).Should(BeTrue())
 			})
 		})
-
-		When("the kube-boat socket exists", func() {
-			It("removes the socket", func() {
-				file, err := os.Create(filepath.Join(home, ".kube-boat", "daemon.socket"))
-				Expect(err).NotTo(HaveOccurred())
-
-				err = sock.Close()
-				Expect(err).NotTo(HaveOccurred())
-
-				_, err = os.Stat(file.Name())
-				Expect(errors.Is(err, fs.ErrNotExist)).Should(BeTrue())
-			})
-		})
 	})
+
 })
