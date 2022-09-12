46d0c2e2e180572826a2d40efae64c4ecb96a0c2
hhiroshell <hhiroshell@gmail.com>
2022-09-12 21:43:03 +0900

    add Readyz() func to daemon client

--- daemon/client_test.go
+++ daemon/client_test.go
@@ -13,6 +13,48 @@ var _ = Describe("Client", func() {
 		httpmock.Reset()
 	})
 
+	Describe("func Readyz()", func() {
+		When("the kube-boat daemon returns 200 OK", func() {
+			It("ends with no error", func() {
+				httpmock.RegisterResponder(
+					http.MethodGet,
+					"http://localhost"+readyz,
+					httpmock.NewStringResponder(http.StatusOK, "ok"),
+				)
+
+				client := &Client{client: &http.Client{}}
+				err := client.Readyz()
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+
+		When("the kube-boat daemon is not running", func() {
+			It("ends up with error", func() {
+				// do not register responder to emulate that the kube-boat daemon is not running
+
+				client := &Client{client: &http.Client{}}
+				err := client.Readyz()
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).Should(HavePrefix("kube-boat daemon or local Kubernetes API Server is not ready:"))
+			})
+		})
+
+		When("the kube-boat daemon returns status code that indicates tha daemon or the API Server is unhealthy", func() {
+			It("ends up with error", func() {
+				httpmock.RegisterResponder(
+					http.MethodGet,
+					"http://localhost"+readyz,
+					httpmock.NewStringResponder(http.StatusInternalServerError, "ng"),
+				)
+
+				client := &Client{client: &http.Client{}}
+				err := client.Readyz()
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).Should(HavePrefix("kube-boat daemon or local Kubernetes API Server is not ready:"))
+			})
+		})
+	})
+
 	Describe("func Kubeconfig()", func() {
 		When("the kube-boat daemon returns correct kubeconfig response", func() {
 			var response = `{
@@ -23,7 +65,7 @@ var _ = Describe("Client", func() {
 			It("should return a correctly unmarshalled result", func() {
 				httpmock.RegisterResponder(
 					http.MethodGet,
-					"http://localhost/kube-boat",
+					"http://localhost"+kubeconfig,
 					httpmock.NewStringResponder(http.StatusOK, response),
 				)
 
@@ -58,7 +100,7 @@ var _ = Describe("Client", func() {
 			It("should return a correctly unmarshalled result", func() {
 				httpmock.RegisterResponder(
 					http.MethodDelete,
-					"http://localhost/kube-boat",
+					"http://localhost"+base,
 					httpmock.NewStringResponder(http.StatusAccepted, response),
 				)
 
