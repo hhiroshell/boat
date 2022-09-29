0bd5af21063c6bebac03f07c46c92ebc891a4467
hhiroshell <hhiroshell@gmail.com>
2022-09-29 22:03:48 +0900

    add status sub command

---
+++ cmd/status.go
@@ -0,0 +1,61 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"syscall"
+
+	"github.com/spf13/cobra"
+
+	"github.com/hhiroshell/boat/daemon"
+)
+
+var statusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Check the status of local Kubernetes API server",
+	Long:  "Check the status of local Kubernetes API server",
+	RunE:  status,
+}
+
+func init() {
+	rootCmd.AddCommand(statusCmd)
+}
+
+const (
+	boatd   = "kube-boat daemon"
+	kubeapi = "Kubernetes API Server"
+)
+
+func status(_ *cobra.Command, _ []string) error {
+	client, err := daemon.NewClient()
+	if err != nil {
+		return err
+	}
+
+	status := map[string]string{
+		boatd:   "⛔ Not yet running",
+		kubeapi: "⛔ Not yet running",
+	}
+	defer func() {
+		for k, v := range status {
+			fmt.Println(k + ": " + v)
+		}
+	}()
+
+	if err := client.Readyz(); err != nil {
+		var errno syscall.Errno
+		if errors.As(err, &errno) {
+			// kube-boat daemon and Kube API Server are not yet running.
+			return nil
+		} else {
+			// Readyz endpoint of Kube API Server returned some unhealthy status.
+			status[boatd] = "✅ Running"
+			status[kubeapi] = "⚠️ Unhealthy"
+			return nil
+		}
+	}
+
+	status[boatd] = "✅ Running"
+	status[kubeapi] = "✅ Running"
+	return nil
+}
